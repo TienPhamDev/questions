bcb3d955a592f3031c780a69f63f1bfc7f1952d0
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-09-16 13:25:37 -0400

    deploy questions

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import questions from "./data";
+import Questions from "./Questions";
+const App = () => {
+  return <main>
+    <Questions questions={questions}/>
+  </main> 
+  
+};
+
+export default App;
